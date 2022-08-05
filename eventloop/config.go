331ae6af0a1791f72c72e5021e5095f15393c1e0
nguyenzung <nguyenvietdungcs52@gmail.com>
2022-08-05 16:19:27 +0700

    add apicall module for get request

---
+++ eventloop/config.go
@@ -0,0 +1,6 @@
+package eventloop
+
+var (
+	API_NUM_THREAD         = 16
+	HTTP_SERVER_NUM_THREAD = 16
+)
--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func timeout2(interval int) {
 	fmt.Println("Timeout 2 is called: ", interval, threadutils.TheadID())
 	eventloop.RemoveTimerTask(timerTask1)
 	if timerTask3 == nil {
-		timerTask3 = eventloop.NewTimerTask(3000, timeout3)
+		timerTask3 = eventloop.MakeTimerTask(3000, timeout3)
 	}
 }
 
@@ -25,11 +25,23 @@ func timeout3(interval int) {
 	fmt.Println("Timeout 3 is called: ", interval, threadutils.TheadID())
 }
 
+func apihandle(res string) {
+	fmt.Println("Response: ", res)
+}
+
+func exception(err error) {
+	fmt.Println("API Call err", err)
+}
+
 func main() {
 	eventloop.NewApp()
 	fmt.Println(" MainThread ID: ", threadutils.TheadID())
 	timerTask3 = nil
-	timerTask1 = eventloop.NewTimerTask(2000, timeout1)
-	eventloop.NewTimerTask(9000, timeout2)
+	timerTask1 = eventloop.MakeTimerTask(2000, timeout1)
+	eventloop.MakeTimerTask(9000, timeout2)
+	eventloop.MakeCallTask("http://localhost:8080", 5, apihandle, exception)
+	eventloop.MakeCallTask("http://localhost:8081", 5, apihandle, exception)
+	eventloop.MakeCallTask("http://localhost:8082", 11, apihandle, exception)
+	eventloop.MakeCallTask("http://localhost:8083", 11, apihandle, exception)
 	eventloop.RunApp()
 }
