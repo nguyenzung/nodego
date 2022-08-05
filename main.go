efd9ed0f133a08c4a2c70cc52fb4bffcd420a3fa
nguyenzung <nguyenvietdungcs52@gmail.com>
2022-08-05 11:26:11 +0700

    add timer to event-loop

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/go-event-loop/eventloop"
+	"golang.org/x/sys/unix"
 )
 
+func timeout1(interval int) {
+	pid := unix.Getpid()
+	fmt.Println("Timeout 1 is called: ", interval, pid)
+}
+
+func timeout2(interval int) {
+	pid := unix.Getpid()
+	fmt.Println("Timeout 2 is called: ", interval, pid)
+}
+
 func main() {
-	eventloop.InitAndRunApp()
+	eventloop.NewApp()
+	pid := unix.Getpid()
+	fmt.Println(" MainThread ID: ", pid)
+	eventloop.NewTimerTask(3000, timeout1)
+	eventloop.NewTimerTask(5000, timeout2)
+	eventloop.RunApp()
 }
