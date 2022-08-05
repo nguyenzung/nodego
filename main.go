9681b0c6ef25862cea61d4f27abf348a699b684e
nguyenzung <nguyenvietdungcs52@gmail.com>
2022-08-05 12:42:46 +0700

    update README, add mutex to protect timerTask in TimerModule

--- main.go
+++ main.go
@@ -7,18 +7,21 @@ import (
 	"github.com/go-event-loop/threadutils"
 )
 
+var timerTask1 *eventloop.TimerTask
+
 func timeout1(interval int) {
 	fmt.Println("Timeout 1 is called: ", interval, threadutils.TheadID())
 }
 
 func timeout2(interval int) {
 	fmt.Println("Timeout 2 is called: ", interval, threadutils.TheadID())
+	eventloop.RemoveTimerTask(timerTask1)
 }
 
 func main() {
 	eventloop.NewApp()
 	fmt.Println(" MainThread ID: ", threadutils.TheadID())
-	eventloop.NewTimerTask(3000, timeout1)
-	eventloop.NewTimerTask(5000, timeout2)
+	timerTask1 = eventloop.NewTimerTask(2000, timeout1)
+	eventloop.NewTimerTask(9000, timeout2)
 	eventloop.RunApp()
 }
